7e270b166ecaf72ab8892971b2772b12945aaa2a
thao-wq <thaod3797@gmail.com>
2026-04-02 10:46:29 +0700

    Add files via upload

---
+++ builder/BedroomBuilder.java
@@ -0,0 +1,51 @@
+package builder;
+
+public class BedroomBuilder {
+    String roomID;
+    String bedType;
+    boolean hasBathroom;
+    boolean hasBalcony;
+    String color;
+    double sizeRoom;
+    boolean hasAirConditioner;
+    boolean hasTV;
+
+    public BedroomBuilder(String roomID, String bedType) {
+        this.roomID = roomID;
+        this.bedType = bedType;
+    }
+
+    public BedroomBuilder withHasBathroom(boolean hbr) {
+        this.hasBathroom = hbr;
+        return this;
+    }
+
+    public BedroomBuilder withHasBalcony(boolean hb) {
+        this.hasBalcony = hb;
+        return this;
+    }
+
+    public BedroomBuilder withColor(String color) {
+        this.color = color;
+        return this;
+    }
+
+    public BedroomBuilder withSizeRoom(double sr) {
+        this.sizeRoom = sr;
+        return this;
+    }
+
+    public BedroomBuilder withHasAirConditioner(boolean hac) {
+        this.hasAirConditioner = hac;
+        return this;
+    }
+
+    public BedroomBuilder withHasTV(boolean htv) {
+        this.hasTV = htv;
+        return this;
+    }
+
+    public Bedroom build() {
+        return new Bedroom(this);
+    }
+}
